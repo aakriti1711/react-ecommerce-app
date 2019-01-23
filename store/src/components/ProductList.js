d28c3c0e66f55dcb645e12b08cc1e902c7901baf
Aakriti <aakriti1711@gmail.com>
2019-01-23 15:01:28 -0800

    Nav Bar Component code added

--- store/src/components/ProductList.js
+++ store/src/components/ProductList.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-
+import Title from './Title';
 import Product from './Product';
 export default class ProductList extends Component {
+  state={
+    products: []
+  }
   render() {
     return (
-      <div>
-        <Product/>
-      </div>
+     <React.Fragment>
+       <div className="py-5">
+         <div className="container">
+           <Title name ="Our" title ="Products" />
+           <div className="row">
+
+           </div>
+         </div>
+       </div>
+     </React.Fragment>
     )
   }
 }
