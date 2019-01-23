4b9565fc2c4f6c219c0539fd6d14f77f673edc59
Aakriti <aakriti1711@gmail.com>
2019-01-23 10:53:04 -0800

    Added component structure

--- store/src/App.js
+++ store/src/App.js
@@ -3,17 +3,28 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Details from './components/Details';
+import Product from './components/Product';
+import ProductList from './components/ProductList';
+
 class App extends Component {
-  render() {
-    return (
-       <div className="containe">
-         <div className="row">
-           <div className="col-6">Column 1</div>
-           <div className="col-6">Column 2</div>
-         </div>
-       </div>
-    );
-  }
+ render(){
+   return <React.Fragment>
+    <Navbar/>
+    <Cart/>
+    <Default/>
+    <Details/>
+    <Product/>
+    <ProductList/>
+   </React.Fragment>
+
+ }
+    
+  
 }
 
 export default App;
---
+++ store/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello From NavBar</h3>
+      </div>
+    )
+  }
+}
