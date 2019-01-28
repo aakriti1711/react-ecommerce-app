bafcb41ab97854af8bd3e6ac8b0e9d1c4668071d
Aakriti <aakriti1711@gmail.com>
2019-01-28 11:10:18 -0800

    changs to context.js

--- store/src/context.js
+++ store/src/context.js
@@ -1,69 +1,87 @@
 import React, { Component } from 'react';
-import {storeProducts, detailProduct} from './data';
+import { storeProducts, detailProduct } from './data';
 
 const ProductContext = React.createContext();
 //Provider
 //Consumer
 
 export default class ProductProvider extends Component {
-    state ={
-        products:[],
-        detailProduct:detailProduct
+    state = {
+        products: [],
+        detailProduct: detailProduct,
+        cart: []
     }
-    componentDidMount(){
+    componentDidMount() {
         this.setProducts();
     }
     setProducts = () => {
         let products = [];
         storeProducts.forEach(item => {
-            const singleItem = {...item};
+            const singleItem = { ...item };
             products = [...products, singleItem];
-            
+
         });
-        this.setState(()=>{
-            return {products}
+        this.setState(() => {
+            return { products }
         });
     }
 
-    getItem = (id) =>{
-            const product = this.state.products.find(item =>  item.id === id);
-            return product;
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
     }
 
-    handleDetail = (id) =>{
+    handleDetail = (id) => {
         const product = this.getItem(id);
-        this.setState(()=>{
-            return {detailProduct : product};
+        this.setState(() => {
+            return { detailProduct: product };
         })
     };
-    addToCart =(id) =>{
-        console.log(`Hello from Addto cart id is $ {id}`);
-    }  
-   /* tester = () =>{
-        console.log('State Products: ',this.state.products[0].inCart);
-        console.log('Data Products: ',storeProducts[0].inCart);
-        const tempProduct = [...this.state.products];
-        tempProduct[0].inCart = true;
-        this.setState(()=>{
-            return {products:tempProduct}
-        },()=>{
-            console.log('State Products: ',this.state.products[0].inCart);
-            console.log('Data Products: ',storeProducts[0].inCart);
-        });
+    addToCart = (id) => {
+        let tempProduct = [...this.state.products];
+        const index = tempProduct.indexOf(this.getItem(id));
+        const product = tempProduct[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
 
-    }*/
+        this.setState(() => {
+            return {
+                products: tempProduct,
+                cart: [...this.state.cart,product]
+                
+            }
+        }, () => {
+            console.log(this.state);
+
+        })
+    }
+    /* tester = () =>{
+         console.log('State Products: ',this.state.products[0].inCart);
+         console.log('Data Products: ',storeProducts[0].inCart);
+         const tempProduct = [...this.state.products];
+         tempProduct[0].inCart = true;
+         this.setState(()=>{
+             return {products:tempProduct}
+         },()=>{
+             console.log('State Products: ',this.state.products[0].inCart);
+             console.log('Data Products: ',storeProducts[0].inCart);
+         });
+ 
+     }*/
     render() {
-    return (
-      <ProductContext.Provider value ={{
-          ...this.state,
-          handleDetail:this.handleDetail,
-          addToCart:this.addToCart
-      }}>
-      {/* <button onClick={this.tester}>Test Me</button> */}
-          {this.props.children}
-      </ProductContext.Provider>
-    )
-  }
+        return (
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
+                {/* <button onClick={this.tester}>Test Me</button> */}
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
 }
 const ProductConsumer = ProductContext.Consumer;
-export {ProductProvider,ProductConsumer};
+export { ProductProvider, ProductConsumer };
